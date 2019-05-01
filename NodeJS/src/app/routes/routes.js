44e3d776048ad74fa2b808df85470aa4f4ca3207
Kayan Silva <kayanf.silva@outlook.com>
2019-04-30 23:30:41 -0300

    Sistema funcionando!!!!

--- NodeJS/src/app/routes/routes.js
+++ NodeJS/src/app/routes/routes.js
@@ -13,14 +13,55 @@ module.exports = (app) => {
           <h1> Casa do Código </h1>
         </body> 
       </html>
-`)
+    `)
   })
+
+  app.get('/livros/form/:id', function (req, resp) {
+    const id = req.params.id
+    const livroDao = new LivroDAO(db)
+
+    livroDao.buscaPorId(id)
+      .then(livro => resp.marko(require('../views/books/form/form.marko'),
+        {
+          livro: livro
+        }))
+      .catch(erro => console.log(erro))
+  })
+
   app.get('/livros', function (req, resp) {
     const livrodao = new LivroDAO(db)
     livrodao.lista()
       .then(livros => resp.marko(require('../views/books/list/lista.marko'),
         {
           livros: livros
-        }).catch(erro => console.log(erro)))
+        }))
+      .catch(erro => console.log(erro))
+  })
+
+  app.get('/livros/form', function (req, resp) {
+    resp.marko(require('../views/books/form/form.marko'),
+      { livro: {} })
+  })
+
+  app.post('/livros', function (req, resp) {
+    const livrodao = new LivroDAO(db)
+    livrodao.adiciona(req.body)
+      .then(resp.redirect('/livros'))
+      .catch(erro => console.log(erro))
+  })
+
+  app.put('/livros', function (req, resp) {
+    const livrodao = new LivroDAO(db)
+    livrodao.atualiza(req.body)
+      .then(resp.redirect('/livros'))
+      .catch(erro => console.log(erro))
+  })
+
+  app.delete('/livros/:id', function (req, resp) {
+    const id = req.params.id
+    const livroDao = new LivroDAO(db)
+    livroDao.remove(id)
+      .then(() => resp.status(200).end())
+      .catch(erro => console.log(erro))
   })
 }
--- NodeJS/src/app/views/books/form/form.marko.js
+++ NodeJS/src/app/views/books/form/form.marko.js
@@ -2,53 +2,44 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/nodejslearning$1.0.0/src/app/views/books/list/lista.marko",
+    marko_componentType = "/nodejslearning$1.0.0/src/app/views/books/form/form.marko",
     components_helpers = require("marko/src/runtime/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
     marko_helpers = require("marko/src/runtime/html/helpers"),
     marko_loadTag = marko_helpers.t,
     component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
-    marko_forEach = marko_helpers.f,
-    marko_escapeXml = marko_helpers.x,
     marko_attr = marko_helpers.a,
+    marko_escapeXml = marko_helpers.x,
     init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
 
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<html><head><meta charset=\"utf-8\"></head><body>");
+  out.w("<html><body>");
 
   component_globals_tag({}, out);
 
-  out.w("<h1> Listagem de livros </h1><table id=\"livros\"><tr><td>ID</td><td>Título</td><td>Preço</td><td>Editar</td><td>Remover</td> </tr>");
+  out.w("<h1>Cadastro de livros</h1><form action=\"/livros\" method=\"post\">");
 
-  var for__12 = 0;
+  if (data.livro.id) {
+    out.w("<div><input type=\"hidden\" name=\"_method\" value=\"PUT\"><input type=\"hidden\" id=\"id\" name=\"id\"" +
+      marko_attr("value", "" + data.livro.id) +
+      "></div>");
+  }
 
-  marko_forEach(data.livros, function(livro) {
-    var keyscope__13 = "[" + ((for__12++) + "]");
-
-    out.w("<tr" +
-      marko_attr("id", "livro_" + livro.id) +
-      "><td>" +
-      marko_escapeXml(livro.id) +
-      "</td><td>" +
-      marko_escapeXml(livro.titulo) +
-      "</td><td>" +
-      marko_escapeXml(livro.preco) +
-      "</td><td><a" +
-      marko_attr("href", "/livros/form/" + livro.id) +
-      ">Editar</a></td><td><a href=\"#\"" +
-      marko_attr("data-ref", "" + livro.id) +
-      " data-type=\"remocao\">Remover</a></td></tr>");
-  });
-
-  out.w("</table><script src=\"/estatico/js/remove-livro.js\"></script>");
+  out.w("<div><label for=\"titulo\">Titulo:</label><input type=\"text\" id=\"titulo\" name=\"titulo\"" +
+    marko_attr("value", "" + data.livro.titulo) +
+    " placeholder=\"coloque o titulo\"></div><div><label for=\"preco\">Preço:</label><input type=\"text\" id=\"preco\" name=\"preco\"" +
+    marko_attr("value", "" + data.livro.preco) +
+    " placeholder=\"150.25\"></div><div><label for=\"descricao\">Descrição:</label><textarea cols=\"20\" rows=\"10\" id=\"descricao\" name=\"descricao\" placeholder=\"fale sobre o livro\">" +
+    marko_escapeXml(data.livro.descricao) +
+    "</textarea></div><input type=\"submit\" value=\"Salvar\"></form>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "23");
+  await_reorderer_tag({}, out, __component, "17");
 
   out.w("</body></html>");
 }
@@ -61,7 +52,7 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/nodejslearning$1.0.0/src/app/views/books/list/lista.marko",
+    id: "/nodejslearning$1.0.0/src/app/views/books/form/form.marko",
     tags: [
       "marko/src/core-tags/components/component-globals-tag",
       "marko/src/core-tags/components/init-components-tag",
